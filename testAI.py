d1b6369af35ddfb6d88c49b2800bf9023d094b68
nguyenphihung2003 <164150623+nguyenphihung2003@users.noreply.github.com>
2024-03-20 15:38:04 +0700

    Update testAI.py

--- testAI.py
+++ testAI.py
@@ -2,29 +2,7 @@ import streamlit as st
 import pickle
 from sklearn.datasets import load_iris
 
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-
-
 iris = load_iris()
-X, y =iris.data, iris.target
-
-print(X.shape)
-#print(y.shape)
-
-X_train, X_test, y_train, y_test =train_test_split(X,y,test_size=0.2)
-print(X_train.shape)
-print(y_train.shape)
-
-clf=RandomForestClassifier()
-clf.fit(X_train, y_train)
-
-print(clf.score(X_test, y_test))
-
-print('Saving model to pickle file')
-pickle.dump(clf, open("iris_model.pkl", 'wb'))
-
-
 
 # Load the trained model
 clf = pickle.load(open('iris_model.pkl', 'rb'))
